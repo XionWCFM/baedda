d1673063bd922cea9108e83c0653b7f2957d6419
devgiljong <calf-love@naver.com>
2023-12-28 00:54:02 +0900

    :sparkles: Add

--- src/routes/root/index.tsx
+++ src/routes/root/index.tsx
@@ -1,19 +1,19 @@
 'use client';
 import { useDarkmode } from '@/src/hooks/darkmode';
-import { Box, Button, Flex, Text } from '@radix-ui/themes';
+import { Box, Button, Flex, Heading, Text } from '@radix-ui/themes';
 import React from 'react';
 
 interface RootPageProps {}
 
 const RootPage = ({}: RootPageProps) => {
-  const [_, darkToggle] = useDarkmode();
   return (
-    <Flex direction={'column'} className=" px-4">
-      <Flex>
-        <Button onClick={() => darkToggle()}>다크모드</Button>
-      </Flex>
-      <Text>안녕하세요</Text>
-      <Box height={'8'} />
+    <Flex
+      direction={'column'}
+      align={'center'}
+      className="px-4 min-h-screen pt-40"
+    >
+      <Heading>Hello Baedda people</Heading>
+      
     </Flex>
   );
 };
