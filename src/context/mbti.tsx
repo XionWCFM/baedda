5fd4b55bac5f5b900dff610bd3b6039663f6bf20
devgiljong <calf-love@naver.com>
2023-12-28 01:22:51 +0900

    :sparkles: Context api 생성

---
+++ src/context/mbti.tsx
@@ -0,0 +1,32 @@
+import { createContext, useContext } from 'react';
+
+
+
+export const MbtiContext = createContext<null>(null);
+
+export const MbtiDispatchContext = createContext<null>(null);
+
+export const useMbtiContext = () => {
+  const value = useContext(MbtiContext);
+  if (value === null) throw new Error('컨텍스트가 있는 환경에서 써주세요');
+  return value;
+};
+export const useMbtiDispatchContext = () => {
+  const value = useContext(MbtiDispatchContext);
+  if (value === null) throw new Error('컨텍스트가 있는 환경에서 써주세요');
+  return value;
+};
+
+export const MbtiContextProvider = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}) => {
+  return (
+    <MbtiContext.Provider value={null}>
+      <MbtiDispatchContext.Provider value={null}>
+        {children}
+      </MbtiDispatchContext.Provider>
+    </MbtiContext.Provider>
+  );
+};
