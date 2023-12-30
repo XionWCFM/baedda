2b3429ee516c0fe873f3f7ddb154cf4235f203aa
devgiljong <calf-love@naver.com>
2023-12-30 12:55:28 +0900

    :sparkles: Add

--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,9 +1,10 @@
+import QuestionPage from '@/src/routes/question';
 import React from 'react';
 
 interface pageProps {}
 
 const page = ({}: pageProps) => {
-  return <div>page</div>;
+  return <QuestionPage />;
 };
 
 export default page;
---
+++ src/routes/question/index.tsx
@@ -0,0 +1,21 @@
+import { Flex } from '@radix-ui/themes';
+import React from 'react';
+
+interface indexProps {}
+
+const QuestionPage = ({}: indexProps) => {
+  return (
+    <Flex
+      direction={'column'}
+      align={'center'}
+      justify={'between'}
+      className=" min-h-screen min-w-full  max-h-screen pt-16 pb-12"
+    >
+      <Flex direction={'column'} align={'center'}>
+        dsadas
+      </Flex>
+    </Flex>
+  );
+};
+
+export default QuestionPage;
