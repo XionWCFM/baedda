291ecb2042333fc1403079c5ba58b19c243af0a6
devgiljong <calf-love@naver.com>
2024-01-06 16:31:24 +0900

    :sparkles: Provider wrapping

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { MbtiContextProvider } from '@/src/context/mbti';
+import { ResponseProvider } from '@/src/context/response';
 import { Theme } from '@radix-ui/themes';
 import { ThemeProvider } from 'next-themes';
 import React from 'react';
@@ -8,7 +10,11 @@ interface ProvidersProps extends React.PropsWithChildren {}
 const Providers = ({ children }: ProvidersProps) => {
   return (
     <ThemeProvider attribute="class" enableSystem defaultTheme="system">
-      <Theme accentColor={'red'}>{children}</Theme>
+      <Theme accentColor={'red'}>
+        <MbtiContextProvider>
+          <ResponseProvider>{children}</ResponseProvider>
+        </MbtiContextProvider>
+      </Theme>
     </ThemeProvider>
   );
 };
