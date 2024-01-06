9fa3bc31986cc24fa4a242d504c02825cf637021
devgiljong <calf-love@naver.com>
2024-01-06 16:32:08 +0900

    :recycle: Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import '@radix-ui/themes/styles.css';
 import Providers from '@/src/components/providers';
-import { Box, Flex } from '@radix-ui/themes';
-import { MbtiContextProvider } from '@/src/context/mbti';
-import { UserContextProvider } from '@/src/context/user';
+import { Flex } from '@radix-ui/themes';
 export const metadata: Metadata = {
   title: 'baedda',
   description: 'baedda',
@@ -19,19 +17,15 @@ export default async function RootLayout({
     <html suppressHydrationWarning lang="en">
       <body className=" min-h-screen   transition-colors  duration-500">
         <Providers>
-          <MbtiContextProvider>
-            <UserContextProvider>
-              <Flex
-                direction={'column'}
-                align={'center'}
-                className=" bg-slate-200 dark:bg-slate-200 min-h-screen"
-              >
-                <Flex className=" max-w-[430px] w-full bg-white min-h-screen dark:bg-neutral-800">
-                  {children}
-                </Flex>
-              </Flex>
-            </UserContextProvider>
-          </MbtiContextProvider>
+          <Flex
+            direction={'column'}
+            align={'center'}
+            className=" bg-slate-200 dark:bg-slate-200 min-h-screen"
+          >
+            <Flex className=" max-w-[430px] w-full bg-white min-h-screen dark:bg-neutral-800">
+              {children}
+            </Flex>
+          </Flex>
         </Providers>
       </body>
     </html>
